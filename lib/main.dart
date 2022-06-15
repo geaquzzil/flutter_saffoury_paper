ba13f39f6393b1aea69d0b6266acb653c6ce7e9c
Qussai Al-Saffoury <geaquzzil@gmail.com>
2022-06-16 00:26:18 +0300

    main

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_saffoury_paper/main.reflectable.dart';
 import 'package:flutter_saffoury_paper/models/products/products.dart';
 import 'package:flutter_saffoury_paper/models/products/sizes.dart';
-import 'package:flutter_saffoury_paper/tests.dart';
+import 'package:flutter_saffoury_paper/testsAnimiatedList.dart';
 import 'package:flutter_view_controller/models/view_abstract.dart';
 import 'package:flutter_view_controller/providers/cart_provider.dart';
 import 'package:flutter_view_controller/providers/list_provider.dart';
