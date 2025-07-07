c3eade15a4e7938b4e194a024403a16686e13262
Qussai <geaquzzil@gmail.com>
2025-07-07 16:42:23 -0700

    worked well

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\flutter_saffoury_paper"
+export "FLUTTER_ROOT=C:\Users\ETQ\AppData\Local\Flutter\SDK\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\Development\flutter_saffoury_paper"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
